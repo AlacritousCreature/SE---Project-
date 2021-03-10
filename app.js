d4cfe7ed2cee868d8a672c81b6214b6daddfaf7d
AlacritousCreature <IIT2019007@iiita.ac.in>
2021-03-10 20:01:58 +0530

    intial setup

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express"),
+    mongoose = require("mongoose"),
+    //passport = require("passport"),
+    //LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    bodyParser = require("body-parser");
+
+const app = express();
+require('dotenv').config();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.set('useUnifiedTopology', true);
+const url = process.env.MONGODB_URI || 3000
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, () => {
+    console.log("Connected to database.");
+});
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+/*app.use(require("express-session")({
+    secret: "oh! bhaieee, kya majak ho rha hai ye????",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());*/
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/emergencyContacts", (req, res) => {
+    res.render("emergencyContacts");
+});
+
+app.get("/buildingDetails", (req, res) => {
+    res.render("buildingDetails");
+});
+
+app.get("/history", (req, res) => {
+    res.render("history");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+let port = process.env.PORT || 3000
+app.listen(port, process.env.IP, () => {
+    console.log("showing on port 3000.");
+});
